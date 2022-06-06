fabff5af23cb27a2812f037ddf383e32e5777346
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-06-06 15:59:31 +0300

    Example with while

---
+++ index.js
@@ -0,0 +1,57 @@
+//! Цыкл while
+/*
+while (condition) {
+  //iteration
+}
+*/
+
+//? Example 1
+
+// const MAX_SIZE = 10;
+// let count = 0;
+
+/*
+while (count <= MAX_SIZE) {
+  if (count % 2 === 1) {
+    console.log(count);
+  }
+  count++;
+}
+*/
+
+/*
+while (true) {
+  if (count % 2 === 0) {
+    count++;
+    continue;
+  }
+  
+  console.log(count++);
+  if (count >= MAX_SIZE) {
+    break;
+  }
+}
+*/
+
+//? Example 2
+
+/*
+const PIN = "1111";
+const MAX_TRY = 3;
+let countTry = 1;
+
+while (true) {
+  let userInputPin = prompt("Input your PIN: ");
+  if (countTry < MAX_TRY) {
+    countTry++;
+
+    if (userInputPin === PIN) {
+      console.log(userInputPin);
+      break;
+    }
+  } else {
+    console.log("Try later!");
+    break;
+  }
+}
+*/
